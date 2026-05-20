526bfb81ebc6ae5ef7889f60059908980636fbe0
jeimy natalia astaiza <jeimynataliaastaiza@gmail.com>
2026-05-20 16:05:17 -0500

    suboarchibos

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -51,9 +51,15 @@ class AuthService {
     return prefs.getString('nombre_usuario') ?? 'Usuario';
   }
 
+  static Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token');
+  }
+
   // CERRAR SESIÓN
   static Future<void> logout() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove('nombre_usuario');
+    await prefs.remove('token');
   }
 }
